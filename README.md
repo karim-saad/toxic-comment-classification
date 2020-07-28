8a921b38bbd0be0f1d16c42259a8b457257022ec
Karim <z5209523@ad.unsw.edu.au>
2020-07-29 00:48:38 +1000

    dataset unzips, requirements

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # COMP9417 - Final Project
-Mimi Tran - z5158339\
+Mimi Tran - z5158339
+
 Karim Saad - z5209523
---
+++ toxic.py
@@ -0,0 +1,22 @@
+import os
+from zipfile import ZipFile
+
+
+def unzip_files():
+    if not os.path.isdir('train'):
+        new_dir = os.path.join(os.getcwd(), 'train')
+        os.mkdir(new_dir)
+        ZipFile('train.csv.zip', 'r').extractall(new_dir)
+
+    if not os.path.isdir('test'):
+        new_dir = os.path.join(os.getcwd(), 'test')
+        os.mkdir(new_dir)
+        ZipFile('test.csv.zip', 'r').extractall(new_dir)
+
+    if not os.path.isdir('test_labels'):
+        new_dir = os.path.join(os.getcwd(), 'test_labels')
+        os.mkdir(new_dir)
+        ZipFile('test_labels.csv.zip', 'r').extractall(new_dir)
+
+
+unzip_files()
