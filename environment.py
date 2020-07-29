a9691379f372c82c83048d39b132340ea3c8fe40
Karim <z5209523@ad.unsw.edu.au>
2020-07-29 11:23:01 +1000

    restructuring + cleaning code

--- environment.py
+++ environment.py
@@ -3,27 +3,31 @@ from zipfile import ZipFile
 import pandas as pd
 
 
+cwd = os.getcwd()
+
+
 def environment():
     unzip_files()
     return csv_to_dataframe()
 
 
 def unzip_files():
-    if not os.path.isfile('train.csv'):
-        ZipFile('train.csv.zip', 'r').extractall(os.getcwd())
+    if not os.path.isfile('data/train.csv'):
+        ZipFile('data/train.csv.zip', 'r').extractall(os.path.join(cwd, 'data'))
 
-    if not os.path.isfile('test.csv'):
-        ZipFile('test.csv.zip', 'r').extractall(os.getcwd())
+    if not os.path.isfile('data/test.csv'):
+        ZipFile('data/test.csv.zip', 'r').extractall(os.path.join(cwd, 'data'))
 
-    if not os.path.isfile('test_labels.csv'):
-        ZipFile('test_labels.csv.zip', 'r').extractall(os.getcwd())
+    if not os.path.isfile('data/test_labels.csv'):
+        ZipFile('data/test_labels.csv.zip',
+                'r').extractall(os.path.join(cwd, 'data'))
 
 
 def csv_to_dataframe():
-    train_filename = os.path.join(os.getcwd(), 'train.csv')
+    train_filename = os.path.join(cwd, 'data', 'train.csv')
     train = pd.read_csv(train_filename)
-    test_filename = os.path.join(os.getcwd(), 'test.csv')
+    test_filename = os.path.join(cwd, 'data', 'test.csv')
     test = pd.read_csv(test_filename)
-    test_labels_filename = os.path.join(os.getcwd(), 'test_labels.csv')
+    test_labels_filename = os.path.join(cwd, 'data', 'test_labels.csv')
     test_labels = pd.read_csv(test_labels_filename)
     return train, test, test_labels
