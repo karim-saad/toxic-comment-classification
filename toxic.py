3155a58a4f80455fce4f8d11c5f9d6e1d977977d
Karim <z5209523@ad.unsw.edu.au>
2020-07-29 10:28:18 +1000

    environment update

--- toxic.py
+++ toxic.py
@@ -3,20 +3,14 @@ from zipfile import ZipFile
 
 
 def unzip_files():
-    if not os.path.isdir('train'):
-        new_dir = os.path.join(os.getcwd(), 'train')
-        os.mkdir(new_dir)
-        ZipFile('train.csv.zip', 'r').extractall(new_dir)
+    if not os.path.isdir('train.csv'):
+        ZipFile('train.csv.zip', 'r').extractall(os.getcwd())
 
-    if not os.path.isdir('test'):
-        new_dir = os.path.join(os.getcwd(), 'test')
-        os.mkdir(new_dir)
-        ZipFile('test.csv.zip', 'r').extractall(new_dir)
+    if not os.path.isdir('test.csv'):
+        ZipFile('test.csv.zip', 'r').extractall(os.getcwd())
 
-    if not os.path.isdir('test_labels'):
-        new_dir = os.path.join(os.getcwd(), 'test_labels')
-        os.mkdir(new_dir)
-        ZipFile('test_labels.csv.zip', 'r').extractall(new_dir)
+    if not os.path.isdir('test_labels.csv'):
+        ZipFile('test_labels.csv.zip', 'r').extractall(os.getcwd())
 
 
 unzip_files()
