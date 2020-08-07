3aa40105576d228dd3989fdddc22f941dc021243
Karim <z5209523@ad.unsw.edu.au>
2020-08-07 16:18:00 +1000

    updates

--- basic_keras.py
+++ basic_keras.py
@@ -1,7 +1,42 @@
-#from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, GlobalMaxPool1D
+from keras.models import Model
+from keras import initializers, regularizers, constraints, optimizers, layers
 
 
-def basic_keras(train, test, test_labels):
+def basic_keras(train, test):
+    y = train[list(train.columns)[2:]].values
+
     max_features = 2000
     tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(train['comment_text']))
+    tokenized_train = tokenizer.texts_to_sequences(train['comment_text'])
+    tokenized_test = tokenizer.texts_to_sequences(test['comment_text'])
+
+    max_length = 200
+    x_train = pad_sequences(tokenized_train, maxlen=max_length)
+    x_test = pad_sequences(tokenized_test, maxlen=max_length)
+    input_layer = Input(shape=(max_length, ))
+
+    embedding_size = 128
+    x = Embedding(max_features, embedding_size)(input_layer)
+    x = LSTM(60, return_sequences=True, name='lstm_layer')(x)
+    x = GlobalMaxPool1D()(x)
+    x = Dropout(0.1)(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(6, activation='sigmoid')(x)
+
+    model = Model(inputs=input_layer, outputs=x)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics='accuracy')
+
+    batch_size = 32
+    epochs = 2
+    model.fit(x_train, y, batch_size=batch_size,
+              epochs=epochs, validation_split=0.1)
+
+    sub = pd.read_csv('submissions/sample_submission.csv')
+    sub[classes] = model.predict([x_test], batch_size=1024, verbose=1)
+    sub.to_csv('submissions/keras_submission.csv', index=False)
