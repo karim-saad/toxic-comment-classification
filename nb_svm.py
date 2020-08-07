4d7586e61257dd97b2bcc3d416e4d84905330e07
Karim <z5209523@ad.unsw.edu.au>
2020-08-07 17:38:05 +1000

    mimi suck

---
+++ nb_svm.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import cross_val_score
+import re
+import string
+
+
+def nb_svm(train, test):
+    length = train.comment_text.str.len()
+    print(f'Mean length = {length.mean()}')
+    print(f'Standard deviation = {length.std()}')
+    print(f'Max length = {length.max()}')
+
+    label_cols = list(train.columns)[2:]
+    train['none'] = 1 - train[label_cols].max(axis=1)
+
+    train['comment_text'].fillna('unknown', inplace=True)
+    test['comment_text'].fillna('unknown', inplace=True)
+
+    vec = TfidfVectorizer(ngram_range=(1, 2), tokenizer=tokenize, min_df=3,
+                          max_df=0.9, strip_accents='unicode', use_idf=1, smooth_idf=1, sublinear_tf=1)
+    x = vec.fit_transform(train['comment_text'])
+    x_test = vec.transform(test['comment_text'])
+
+    preds = np.zeros((len(test), len(label_cols)))
+
+    for count, class_name in enumerate(label_cols):
+        print(f'Fit {class_name}')
+        m, r = get_mdl(train[class_name], x)
+        print(np.mean(cross_val_score(m, x, train[class_name])))
+        preds[:, count] = m.predict_proba(x_test.multiply(r))[:, 1]
+
+
+def tokenize(s):
+    re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+    return re_tok.sub(r' \1 ', s).split()
+
+
+def pr(y_i, y, x):
+    p = x[y == y_i].sum(0)
+    return (p+1)/((y == y_i).sum()+1)
+
+
+def get_mdl(y, x):
+    y = y.values
+    r = np.log(pr(1, y, x)/pr(0, y, x))
+    m = LogisticRegression(C=4, dual=True, solver='liblinear', max_iter=1000)
+    x_nb = x.multiply(r)
+    return m.fit(x_nb, y), r
--- toxic.py
+++ toxic.py
@@ -1,10 +1,12 @@
 from environment import environment
 from explore import explore
-from basic_keras import basic_keras
+#from basic_keras import basic_keras
 #from basic_logistic import basic_logistic
+from nb_svm import nb_svm
 
 if __name__ == '__main__':
     train, test, test_labels = environment()
     #explore(train, test)
-    basic_keras(train, test)
+    #basic_keras(train, test)
     #basic_logistic(train, test, test_labels)
+    nb_svm(train, test)
